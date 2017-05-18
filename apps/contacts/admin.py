78e824ff68ffccf459c161f8e01cf8b98c2c4c45
Robert Węglarek <weglarek.robert@gmail.com>
2017-05-18 22:00:08 +0200

    Add Entry model + admin

--- apps/contacts/admin.py
+++ apps/contacts/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 
-from .models import AddressBook
+from .models import AddressBook, Entry
 
 
 @admin.register(AddressBook)
 class AddressBookAdmin(admin.ModelAdmin):
     pass
+
+
+@admin.register(Entry)
+class EntryAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'owner_username', 'email')
+
+    def owner_username(self, obj):
+        return obj.address_book.owner.username
+    owner_username.short_description = 'Owner'
--- apps/contacts/models.py
+++ apps/contacts/models.py
@@ -4,3 +4,21 @@ from django.db import models
 
 class AddressBook(models.Model):
     owner = models.OneToOneField(settings.AUTH_USER_MODEL)
+
+    def __str__(self):
+        return self.owner.username
+
+
+class Entry(models.Model):
+    address_book = models.ForeignKey(AddressBook)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    mobile_number = models.CharField(max_length=15)
+    address = models.TextField()
+    email = models.EmailField()
+
+    class Meta:
+        verbose_name_plural = 'Entries'
+
+    def __str__(self):
+        return '{} {}'.format(self.first_name, self.last_name)
